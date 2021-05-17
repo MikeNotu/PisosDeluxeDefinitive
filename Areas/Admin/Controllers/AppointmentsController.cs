d86258fc43d85075afbd5c8e71b7af7230c4bd96
MikeNotu <mikebateman98@gmail.com>
2021-05-17 06:26:43 -0500

    Added sales person to appointment

---
+++ Areas/Admin/Controllers/AppointmentsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PisosDeluxeDefinitive.Utility;
+
+namespace PisosDeluxeDefinitive.Areas.Admin.Controllers
+{
+    [Authorize(Roles = SD.AdminEndUser + "," + SD.SuperAdminEndUser)]
+    [Area("Admin")]
+    public class AppointmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ViewModel/AppointmentsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PisosDeluxeDefinitive.Models.ViewModel
+{
+    public class AppointmentsViewModel
+    {
+        public List<Appointments> Appointments { get; set; }
+
+    }
+}
